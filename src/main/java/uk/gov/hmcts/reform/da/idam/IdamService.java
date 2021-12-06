8e94755c9f2d9d6a16deb77200b78e6e39a728e7
Aloknath Datta <alokdatta.uit@gmail.com>
2021-12-06 15:42:55 +0000

    Updated secrets

--- src/main/java/uk/gov/hmcts/reform/da/idam/IdamService.java
+++ src/main/java/uk/gov/hmcts/reform/da/idam/IdamService.java
@@ -3,12 +3,14 @@ package uk.gov.hmcts.reform.da.idam;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.models.User;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import static uk.gov.hmcts.reform.da.common.config.ControllerConstants.BEARER_PREFIX;
 
+@Service
 public class IdamService {
 
     @Value("${idam.systemupdate.username}")
