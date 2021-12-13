ab3eab60235b48f56973609cb139e57edead733d
Aloknath Datta <alokdatta.uit@gmail.com>
2021-12-13 13:46:12 +0000

    Update user roles

--- bin/create-solicitor-user.sh
+++ bin/create-solicitor-user.sh
@@ -7,7 +7,7 @@ USER_EMAIL="${2:-solicitor@server.net}"
 ENV="${3:-local}"
 FORENAME="${4:-John}"
 SURNAME="${5:-Smith}"
-USER_ROLES='[{"code":"caseworker-da"},{"code":"caseworker-da-solicitor"},{"code":"caseworker"}]'
+USER_ROLES='[{"code":"caseworker"},{"code":"caseworker-domesticabuse"},{"code":"caseworker-domesticabuse-solicitor"}]'
 
 binFolder=$(dirname "$0")
 
